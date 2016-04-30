7d8e19d7dfa68874de1efa357a66f3e1561cec19
Alexandros Callos <ajcallos@asu.edu>
2016-04-29 20:46:06 -0700

    Cleaned up unit tests to work with new ship class structure

--- src/ship/Battleship.java
+++ src/ship/Battleship.java
@@ -2,13 +2,10 @@ package ship;
 
 class Battleship extends Ship {
 	
-	public final static int BATTLESHIP_SIZE = 4;
-	
 	public Battleship () {
 		super();
 		super.myDirection = DEFAULT_DIRECTION;
 		super.hits = INITIAL_HITS;
-		super.size = BATTLESHIP_SIZE;
 		super.myType = ShipType.BATTLESHIP;
 		super.x = 0;
 		super.y = 0;
