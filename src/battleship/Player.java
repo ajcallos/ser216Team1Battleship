910038551932ab693377fe920e3d43b5857e051c
Andrew Negri <andrewnegri1+git@gmail.com>
2016-04-29 23:05:22 -0700

    Revert "Completed textValidPlace() and made relevant fixes in Player"
    This reverts commit e5317c88b6916d2654f28a2e5d38238b08286716.

--- src/battleship/Player.java
+++ src/battleship/Player.java
@@ -224,46 +224,46 @@ public class Player {
 	// find if the ship will be off the board 
 	public boolean validPlace(int x, int y) {
 		if (currentShip.getType() == ShipType.AIRCRAFT_CARRIER) {
-			if (shipDirection == ShipDirection.VERTICAL && y <= 6 && y >= 1) {
+			if (shipDirection.equals("Vertical") && y <= 6) {
 				return true;
 			}
-			if (shipDirection == ShipDirection.HORIZONTAL && x <= 6 && x >= 1) {
+			if (shipDirection.equals("Horizontal") && x <= 6) {
 				return true;
 			}
 		}
 
 		if (currentShip.getType() == ShipType.BATTLESHIP) {
-			if (shipDirection == ShipDirection.VERTICAL && y <= 7 && y >= 1) {
+			if (shipDirection.equals("Vertical") && y <= 7) {
 				return true;
 			}
-			if (shipDirection == ShipDirection.HORIZONTAL && x <= 7 && x >= 1) {
+			if (shipDirection.equals("Horizontal") && x <= 7) {
 				return true;
 			}
 		}
 
 		if (currentShip.getType() == ShipType.SUBMARINE) {
-			if (shipDirection == ShipDirection.VERTICAL && y <= 8 && y >= 1) {
+			if (shipDirection.equals("Vertical") && y <= 8) {
 				return true;
 			}
-			if (shipDirection == ShipDirection.HORIZONTAL && x <= 8 && x >= 1) {
+			if (shipDirection.equals("Horizontal") && x <= 8) {
 				return true;
 			}
 		}
 
 		if (currentShip.getType() == ShipType.DESTROYER) {
-			if (shipDirection == ShipDirection.VERTICAL && y <= 8 && y >= 1) {
+			if (shipDirection.equals("Vertical") && y <= 8) {
 				return true;
 			}
-			if (shipDirection == ShipDirection.HORIZONTAL && x <= 8 && x >= 1) {
+			if (shipDirection.equals("Horizontal") && x <= 8) {
 				return true;
 			}
 		}
 
 		if (currentShip.getType() == ShipType.PATROL_BOAT) {
-			if (shipDirection == ShipDirection.VERTICAL && y <= 9 && y >= 1) {
+			if (shipDirection.equals("Vertical") && y <= 9) {
 				return true;
 			}
-			if (shipDirection == ShipDirection.HORIZONTAL && x <= 9 && x >= 1) {
+			if (shipDirection.equals("Horizontal") && x <= 9) {
 				return true;
 			}
 		}
